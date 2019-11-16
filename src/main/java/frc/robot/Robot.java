f2b278ecda936403cbc4084262ef53e2981143eb
Luis Carrillo <65725@students.mhusd.org>
2019-11-15 17:58:37 -0800

    set up new variables and begin setting values

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,17 +1,32 @@
 package frc.robot;
 
-import com.revrobotics.CANSparkMax;
-import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 
+import java.text.SimpleDateFormat;
+
 public class Robot extends TimedRobot {
-// Construction of framework for code
-  private CANSparkMax m_leftFront;
-  private CANSparkMax m_leftAft;
-  private CANSparkMax m_rightFront;
-  private CANSparkMax m_rightAft;
+    // Construction of framework for code
+    private WPI_VictorSPX m_leftFront;
+    private WPI_VictorSPX m_leftAft;
+    private WPI_VictorSPX m_rightFront;
+    private WPI_VictorSPX m_rightAft;
 
-  private SpeedControllerGroup m_left;
-  private SpeedControllerGroup m_right;
+    private Joystick m_joy;
 
+    private StringBuilder _sb = new StringBuilder();
+    private int looperCounter = 0;
+    private SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yy HH:mm:ss");
+
+public void robotInit() {
+
+    StringBuilder _initSb = new StringBuilder();
+
+    m_leftFront = new WPI_VictorSPX(0);
+    m_leftAft = new WPI_VictorSPX (1);
+    m_rightFront = new WPI_VictorSPX (2);
+    m_rightAft = new WPI_VictorSPX(3);
+    }
 }
